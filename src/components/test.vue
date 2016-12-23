950b1f1e382a4bc0590498aaa6639d46ea2faada
bkmxer <ilchuk.anton@gmail.com>
2016-12-23 17:49:28 +0200

    1st commit

---
+++ src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+        <div>{{msg}}</div>
+
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ffffff;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+        }
+    }
+</script>
